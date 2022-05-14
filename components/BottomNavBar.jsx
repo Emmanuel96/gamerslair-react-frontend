46c14d76ef36ccb21515685a4830d5708cadf9d5
ChenemiKen <chenemiken15@gmail.com>
2022-05-14 12:54:05 +0100

    added inner shadow effect to active navbar item

--- components/BottomNavBar.jsx
+++ components/BottomNavBar.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import { StyleSheet, View, Image,TouchableOpacity} from 'react-native';
+import { ShadowBox, NeomorphBox } from 'react-native-neomorph-shadows';  // https://www.npmjs.com/package/react-native-neomorph-shadows/v/0.0.8
 
 import { baseStyles } from '../style';
 
@@ -7,23 +8,41 @@ export default function BottomNavBar(){
     return(
         <View style={[baseStyles.shadowProp, styles.bottomNavbar]}>
             <View style={[styles.navbar_inner]}>
-                <TouchableOpacity style={[styles.navitem,  styles.active]}>
-                    <Image
-                        source={require('../asset/icons/home.png')}
-                        style={[styles.navicon]}
-                    />
+                <TouchableOpacity style={[styles.navitem]}>
+                    <ShadowBox
+                        inner 
+                        useSvg
+                        style={{...styles.shadowBox, ...styles.active}}
+                    >
+                        <Image
+                            source={require('../asset/icons/home.png')}
+                            style={[styles.navicon]}
+                        />
+                    </ShadowBox>
                 </TouchableOpacity>
                 <TouchableOpacity style={[styles.navitem]}>
-                    <Image
-                        source={require('../asset/icons/plus.png')}
-                        style={[styles.navicon]}
-                    />
+                    <ShadowBox
+                        inner 
+                        useSvg
+                        style={styles.shadowBox}
+                    >
+                        <Image
+                            source={require('../asset/icons/plus.png')}
+                            style={[styles.navicon]}
+                        />
+                    </ShadowBox>
                 </TouchableOpacity>
                 <TouchableOpacity style={[styles.navitem]}>
-                    <Image
-                        source={require('../asset/icons/gamepad.png')}
-                        style={[styles.navicon]}
-                    />
+                    <ShadowBox
+                        inner 
+                        useSvg
+                        style={styles.shadowBox}
+                    >
+                        <Image
+                            source={require('../asset/icons/gamepad.png')}
+                            style={[styles.navicon]}
+                        />
+                    </ShadowBox>
                 </TouchableOpacity>
             </View>
         </View>
@@ -38,7 +57,7 @@ const styles = StyleSheet.create({
         width:'100%',
         borderTopLeftRadius:20,
         borderTopRightRadius:20,
-        shadowOffset:{width: -2, height: -5},
+        shadowOffset:{width: -2, height: -4},
     },
     navbar_inner:{
         flexDirection:'row',
@@ -49,17 +68,28 @@ const styles = StyleSheet.create({
 
     },
     navitem:{
-        paddingHorizontal:40,
-        paddingBottom:10,
-        paddingTop:17,
         borderTopLeftRadius:20,
         borderTopRightRadius:20,    
+    },
+    shadowBox:{
+        paddingTop:4,
+        borderTopLeftRadius:20,
+        borderTopRightRadius:20,
+        width:100,
+        height:58,
+        justifyContent:'center',
+        alignItems:'center',
     },
     navicon:{
         width:30,
         height:30,
     },
     active:{
+        backgroundColor:'rgba(229, 179, 0, 1)',
+        shadowOffset: {width:0, height: 5},
+        shadowOpacity: 0.7,
+        shadowColor: "black",
+        shadowRadius: 4,
         backgroundColor:'rgba(229, 179, 0, 1)',
     },
 
