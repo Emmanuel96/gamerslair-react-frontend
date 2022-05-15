a31576d5b7b46773484d712f0d0d94753c9354b6
ChenemiKen <chenemiken15@gmail.com>
2022-05-15 02:59:11 +0100

    color fix on pagetitlebar

--- components/PageTitleBar.jsx
+++ components/PageTitleBar.jsx
@@ -6,7 +6,7 @@ import { baseStyles } from '../style';
 export default function PageTitleBar(props){
     return(
         <View style={[baseStyles.shadowProp, styles.bar]}>
-            <Text style={[baseStyles.bold, baseStyles.textShadowProp]}>{props.title}</Text>
+            <Text style={[baseStyles.bold, baseStyles.textShadowProp, baseStyles.customColor]}>{props.title}</Text>
         </View>
     )
 }
