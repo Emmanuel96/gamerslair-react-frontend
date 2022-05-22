de9c8ee58492372fd3ca977d9ec829830739ef38
Emmanuel96 <emmanuel.audu1@aun.edu.ng>
2022-05-22 20:52:43 +0100

    combined ongoing-games page

--- pages/OngoingGames.jsx
+++ pages/OngoingGames.jsx
@@ -33,20 +33,91 @@ export default function OngoingGames(props) {
 
 
   function OngoingGamesCard(props){
+    const [report, setReport] = useState(false);
+    const [reported, setReported] = useState(false);
+    const [verified, setVerified] = useState(false);
     return(
       <Card price={props.price} user_name={props.user_name} user_dp={props.user_dp}>
-        <View style={styles.details}>
+        {!reported &&
+          <View style={styles.details}>
             <Text style={[baseStyles.h4, baseStyles.customColor]}>DETAILS:</Text>
-            <Text style={[styles.detailsText]}>{props.details}</Text>
-            
-        </View>
-        <CustomButton
-            title="REPORT RESULTS"
+            <Text style={[styles.detailsText]}>{props.details}</Text> 
+          </View>
+        }
+
+        {!verified &&
+          <View>
+            {!reported &&
+              <View>
+                {!report &&
+                  //report results button
+                  <CustomButton
+                      title="REPORT RESULTS"
+                      color="#fff"
+                      backgroundColor='rgba(105, 139, 78, 1)'
+                      style={styles.button}
+                      textStyle={baseStyles.textShadowProp}
+                      onPress={()=>setReport(true)}
+                  />||
+                
+                  // {/* // first verification buttons */}
+                  <View style={styles.buttonGroup}>
+                    <CustomButton
+                      title="I WON"
+                      color="#fff"
+                      backgroundColor='rgba(105, 139, 78, 1)'
+                      style={styles.buttons}
+                      textStyle={baseStyles.textShadowProp}
+                      onPress={()=>setReported(true)}
+                    />
+                    <CustomButton
+                      title="I LOST"
+                      color="#fff"
+                      backgroundColor='rgba(242, 36, 36, 1)'
+                      style={styles.buttons}
+                      textStyle={baseStyles.textShadowProp}
+                      onPress={()=>setReport(false)}
+                    />
+                  </View> 
+                 }
+              </View> ||
+              
+              <View>
+                {/* // second verification buttons */}
+                <View style={styles.details}>
+                    <Text style={[baseStyles.bold, styles.detailsText]}>Lewandoski says he won the game</Text>  
+                </View>
+                <View style={styles.buttonGroup}>
+                  <CustomButton
+                    image={require('../asset/icons/check.png')}
+                    backgroundColor='rgba(105, 139, 78, 1)'
+                    style={styles.buttons}
+                    onPress={()=>setVerified(true)}
+                  />
+                  <CustomButton
+                    image={require('../asset/icons/close.png')}
+                    backgroundColor='rgba(242, 36, 36, 1)'
+                    style={styles.buttons}
+                    onPress={()=>setReported(false)}
+                  />
+                </View>
+              </View>
+            }
+          </View> ||
+
+          <CustomButton
+            title="YOU LOST THIS GAME"
             color="#fff"
-            backgroundColor='rgba(105, 139, 78, 1)'
+            backgroundColor='rgba(242, 36, 36, 1)'
             style={styles.button}
             textStyle={baseStyles.textShadowProp}
-        />        
+            onPress={()=>{
+              setVerified(false);
+              setReported(false);
+              setReport(false);
+            }}
+          />
+        }    
       </Card>
     )
   }
@@ -70,7 +141,18 @@ export default function OngoingGames(props) {
         alignItems:'center',
         marginBottom:15,
         paddingVertical:25,
-        paddingHorizontal:25,
-        
+        paddingHorizontal:25, 
+    },
+    // first verification buttons
+    buttonGroup:{
+      alignSelf:'center',
+      flexDirection:'row',
+      alignItems:'center',
+      marginBottom:15,
+    },
+    buttons:{
+      marginHorizontal:15,
+      paddingHorizontal:20,
+      paddingVertical:13,
     }
   })
